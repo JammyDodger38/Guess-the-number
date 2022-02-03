6abca976f2d5908751ef46f83eb7fb27d42487a8
Vlad Vladimirov <blad20002000@mail.ru>
2022-02-03 13:22:14 +0700

    Сделал второе подзадание 6 урока

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
 'use strict';
 
+let randNum
+
 const isNumber = function(num) {
     return !isNaN(parseFloat(num)) && isFinite(num)
 }
 
-const GuessTheNumber = function (x) {
+const newGame = function() {
+    randNum = GuessTheNumber(Math.floor(Math.random() * 100) + 1, 10)
+    randNum(prompt("Угадайте число от 1 до 100:"))
+}
+
+const GuessTheNumber = function (x, NumTry) {
     return function (y) {
         if (y == null) {
             alert("Игра окончена")
         } else {
             if (isNumber(y)) {
+                NumTry--
                 if (y > x) {
-                    randNum(prompt("Загаданное число меньше. Введите новое число:"))
+                    if (NumTry == 0) {
+                        if (confirm("Попытки закончились, хотите сыграть еще?")) {
+                            newGame()
+                        } else {
+                            alert("До скорой встречи!")
+                        }
+                    } else {
+                        randNum(prompt("Загаданное число меньше, осталось попыток - " + NumTry + ". Введите новое число:"))
+                    }
                 } else if (y < x) {
-                    randNum(prompt("Загаданное число больше. Введите новое число:"))
+                    if (NumTry == 0) {
+                        if (confirm("Попытки закончились, хотите сыграть еще?")) {
+                            newGame()
+                        } else {
+                            alert("До скорой встречи!")
+                        }
+                    } else {
+                        randNum(prompt("Загаданное число больше, осталось попыток - " + NumTry + ". Введите новое число:"))
+                    }
                 } else if (y == x) {
-                    alert("Поздравляю, вы угадали")
+                    if (confirm("Поздравляю, вы угадали! Хотели бы сыграть ещё?")) {
+                        newGame()
+                    } else {
+                        alert("До скорой встречи!")
+                    }
                 }
             } else {
                 randNum(prompt("Введите число:"))
@@ -24,5 +52,4 @@ const GuessTheNumber = function (x) {
     }
 }
 
-let randNum = GuessTheNumber(Math.floor(Math.random() * 100) + 1)
-console.log(randNum(prompt("Угадайте число от 1 до 100:")));
+newGame()
